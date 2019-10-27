dfbe29a81ac31f6c21f667305162c17c7325faa9
Kushagra Sharma <ksharmaxyz@gmail.com>
2019-10-27 11:25:19 -0700

    DDA-TAP-19: Added camera functionality

---
+++ dda-tap/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  dda-tap
+//
+//  Created by Kushagra Sharma on 2019-10-27.
+//  Copyright © 2019 Code the Change. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
