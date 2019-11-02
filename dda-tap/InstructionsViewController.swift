1f626ecb2d64aaa9d4f5849a08a3d82bb2ed7b8d
vtotient <quique.sira@gmail.com>
2019-11-02 13:49:23 -0700

    Merge DDA-TAP-23

---
+++ dda-tap/InstructionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InstructionsViewController.swift
+//  dda-tap
+//
+//  Created by Victor Sira on 2019-10-31.
+//  Copyright © 2019 Code the Change. All rights reserved.
+//
+
+import UIKit
+
+class InstructionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ dda-tap/menu.swift
@@ -0,0 +1,14 @@
+//
+//  menu.swift
+//  dda-tap
+//
+//  Created by Victor Sira on 2019-10-27.
+//  Copyright © 2019 Code the Change. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class menu : UITableView {
+    
+}
