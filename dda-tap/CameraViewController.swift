b12b381f1f2fe090381c478d092c1206ca2c084b
Kushagra Sharma <ksharmaxyz@gmail.com>
2019-11-02 13:49:44 -0700

    Should have default img working correctly now

--- dda-tap/CameraViewController.swift
+++ dda-tap/CameraViewController.swift
@@ -23,6 +23,31 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
         present(imagePickerController, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePickerController.dismiss(animated: true, completion: nil)
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+    func saveImage(imageName: String){
+        let fileManager = FileManager.default
+        //get the image path
+        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
+        //get the image we took with camera
+        let image = imageView.image!
+        let data = UIImagePNGRepresentation(image)
+        //store it in the document directory    fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
+    }
+    
+    func getImage(imageName: String){
+        let fileManager = FileManager.default
+        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
+        if fileManager.fileExists(atPath: imagePath){
+            imageView.image = UIImage(contentsOfFile: imagePath)
+        }else{
+            print("ERROR: Could not find image :(")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        let img: UIImage = UIImage(named: "TakePictureImage")!
