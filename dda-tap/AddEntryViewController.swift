2bc9ae1a56344c4b0d3fd2aa8e24e71e1c2b5770
kzyx <52890070+kzyx@users.noreply.github.com>
2019-11-17 19:00:17 -0800

    DDA-TAP-19A: Added camera functionality to AddEntry view (#42)
    Add camera functionality to the AddEntry page

---
+++ dda-tap/AddEntryViewController.swift
@@ -0,0 +1,70 @@
+//
+//  AddEntryViewController.swift
+//  dda-tap
+//
+//  Created by Kushagra Sharma on 2019-10-27.
+//  Copyright © 2019 Code the Change. All rights reserved.
+//
+
+import UIKit
+
+class AddEntryViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var imagePickerController : UIImagePickerController!
+    
+    
+    @IBOutlet weak var selectedImageView: UIImageView!
+
+    @IBAction func onPhotoButton(_ sender: Any) {
+        imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .camera
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePickerController.dismiss(animated: true, completion: nil)
+        selectedImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+    func saveImage(imageName: String){
+        let _ = FileManager.default
+        //get the image path
+        let _ = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
+        //get the image we took with camera
+        let image = selectedImageView.image!
+        let _ = UIImagePNGRepresentation(image)
+        //store it in the document directory    fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
+    }
+    
+    func getImage(imageName: String){
+        let fileManager = FileManager.default
+        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
+        if fileManager.fileExists(atPath: imagePath){
+            selectedImageView.image = UIImage(contentsOfFile: imagePath)
+        }else{
+            print("ERROR: Could not find image :(")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let img: UIImage = UIImage(named: "TakePictureImage")!
+        selectedImageView.image = UIImage(named: "TakePictureImage")
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
