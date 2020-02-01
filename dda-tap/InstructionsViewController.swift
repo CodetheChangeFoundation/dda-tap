81a7cfad27750d8000bd32d1e73311c4157918d1
Michelle Gu <michelle.gu@thrive.health>
2020-02-01 14:26:26 -0800

    create Instructions page

--- dda-tap/InstructionsViewController.swift
+++ dda-tap/InstructionsViewController.swift
@@ -9,14 +9,40 @@
 import UIKit
 
 class InstructionsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var InstructionsTextView: UITextView!
+    
+    
+    @IBOutlet weak var TapPatternTextView: UITextView!
+    
+    @IBOutlet weak var TextCodeInstruction: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+//        resizeInstructionsTextView(textView: InstructionsTextView)
+        fitTextViewToContent(textView: TapPatternTextView)
+        fitTextViewToContent(textView: TextCodeInstruction)
     }
     
+    func fitTextViewToContent(textView: UITextView) {
+        var newFrame = textView.frame
+        let width = textView.frame.width
+        let newSize = textView.sizeThatFits(CGSize(width: width, height: CGFloat.greatestFiniteMagnitude))
+        newFrame.size = newSize
+        textView.frame = newFrame
+    }
 
+//    func resizeInstructionsTextView(textView: UITextView) {
+//        var newFrame = textView.frame
+//        let height = textView.frame.height
+//        let newSize = textView.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude, height: height))
+//        newFrame.size = newSize
+//        print("new size \(newSize)")
+//        textView.frame = newFrame
+//    }
     /*
     // MARK: - Navigation
 
