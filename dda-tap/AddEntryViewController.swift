2478dd4a67ad2490ef04caaccb5a31ec9691cbf9
kzyx <52890070+kzyx@users.noreply.github.com>
2020-02-01 13:02:19 -0800

    DDA-TAP-49: Added unit test and alerts for missing audio/image files (#54)
    * DDA-TAP-19B: Added camera roll functionality
    
    * DDA-TAP-19B: Hopefully working
    
    * DDA-TAP-19B: Hopefully working
    
    * DDA-TAP-19B: Added files
    
    * Popup functionality working as desired
    
    * Tests working and also save entry button working
    
    * Alert messages working correctly and tests added

--- dda-tap/AddEntryViewController.swift
+++ dda-tap/AddEntryViewController.swift
@@ -20,6 +20,7 @@ class AddEntryViewController: UIViewController, UINavigationControllerDelegate, 
     var audioRecorder: AVAudioRecorder!
     var audioPlayer : AVAudioPlayer?
     var aLabel: UILabel!
+    var audioExists: Bool!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     
@@ -56,7 +57,7 @@ class AddEntryViewController: UIViewController, UINavigationControllerDelegate, 
             self.loadFailUI()
         }
         
-        
+        audioExists = false
     }
     
         func startRecording() {
@@ -89,6 +90,32 @@ class AddEntryViewController: UIViewController, UINavigationControllerDelegate, 
         
     }
     
+    @IBAction func onSaveEntryButton(_ sender: Any) {
+//        var temp : AVAudioPlayer?
+//        var audioClipExists = true
+//        do {
+//        temp = try AVAudioPlayer(contentsOf: AddEntryViewController.getAudioURL())
+//        } catch {
+//            audioClipExists = false
+//        }
+        
+        if (selectedImage == nil && audioExists) {
+            let alert = UIAlertController(title: "No image selected", message: "You need to select an image before you can save!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        } else if (selectedImage == nil && !audioExists) {
+            let alert = UIAlertController(title: "No image or audio clip selected", message: "You need to select an image and audio clip before you can save!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        } else if (selectedImage != nil && !audioExists) {
+            let alert = UIAlertController(title: "No audio clip selected", message: "You need to select an audio clip before you can save!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        
+    }
+
+    
     // destroys audioRecorder object and if successful, allows re-recordings
     func finishRecording(success: Bool) {
         view.backgroundColor = UIColor(red: 0.6, green: 0, blue: 0, alpha: 1)
@@ -98,6 +125,7 @@ class AddEntryViewController: UIViewController, UINavigationControllerDelegate, 
         
         if success {
             navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
+            audioExists = true
         } else {
             
             let ac = UIAlertController(title: "Record failed", message: "There was a problem recording; please try again.", preferredStyle: .alert)
@@ -234,8 +262,6 @@ class AddEntryViewController: UIViewController, UINavigationControllerDelegate, 
         // store file in document directory
 //        fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
         
-        
-        
     }
     
     
@@ -251,241 +277,3 @@ class AddEntryViewController: UIViewController, UINavigationControllerDelegate, 
 
 
 }
-
-//
-//  AddEntryViewController.swift
-//  dda-tap
-//
-//  Created by Kushagra Sharma on 2019-10-27.
-//  Copyright © 2019 Code the Change. All rights reserved.
-//
-
-/*
-import UIKit
-import AVFoundation
-
-class AddEntryViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
-    
-    var imagePickerController : UIImagePickerController!
-    var recordingSession: AVAudioSession!
-    var audioRecorder: AVAudioRecorder!
-    var audioPlayer : AVAudioPlayer?
-    var aLabel: UILabel!
-    @IBOutlet weak var recordButton: UIButton!
-    @IBOutlet weak var playButton: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //        let img: UIImage = UIImage(named: "TakePictureImage")!
-        
-        view.backgroundColor = UIColor(red: 0.6, green: 0, blue: 0, alpha: 1)
-        
-        selectedImageView.image = UIImage(named: "TakePictureImage")
-        
-        recordingSession = AVAudioSession.sharedInstance()
-        // https://stackoverflow.com/questions/27423243/swift-avaudioplayer-wont-play-audio-recorded-with-avaudiorecorder
-        
-        do{
-            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord) // PlayandRecord
-        }catch{
-            
-        }
-        
-        // requests user's  permission to record audio
-        do {
-            //try recordingSession.setCategory(.playAndRecord, mode: .default)
-            try recordingSession.setActive(true)
-            recordingSession.requestRecordPermission() { [unowned self] allowed in
-                DispatchQueue.main.async {
-                    if allowed {
-                        
-                    } else {
-                        self.loadFailUI()
-                    }
-                }
-            }
-        } catch {
-            self.loadFailUI()
-        }
-        
-        
-    }
-    
-    @IBOutlet weak var selectedImageView: UIImageView!
-    func startRecording() {
-        // may change, right now just ensuring background changes color to show user audio is being recorded
-        view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
-        
-        // make a record button
-        recordButton.setTitle("Tap to Stop", for: .normal)
-        
-        // 3
-        var audioURL = AddEntryViewController.getAudioURL()
-        print(audioURL.absoluteString)
-        
-        // 4
-        let settings = [
-            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-            AVSampleRateKey: 12000,
-            AVNumberOfChannelsKey: 1,
-            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-        ]
-        
-        do {
-            // 5
-            audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
-            audioRecorder.delegate = self
-            let recording = audioRecorder.record(forDuration: 60) // ensures only records for 1 min max.
-        } catch {
-            finishRecording(success: false)
-        }
-        
-    }
-    
-    // destroys audioRecorder object and if successful, allows re-recordings
-    func finishRecording(success: Bool) {
-        view.backgroundColor = UIColor(red: 0.6, green: 0, blue: 0, alpha: 1)
-        
-        audioRecorder.stop()
-        audioRecorder = nil
-        
-        if success {
-            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
-        } else {
-            
-            let ac = UIAlertController(title: "Record failed", message: "There was a problem recording; please try again.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        }
-    }
-    
-    
-    // depending on current state, will allow recordings
-    @objc func nextTapped() {
-        
-    }
-    
-    @IBAction func recordAudio(_ sender: UIButton) {
-        recordTapped()
-    }
-    
-    @objc func recordTapped() {
-        if audioRecorder == nil {
-            startRecording()
-        } else {
-            finishRecording(success: true)
-        }
-    }
-    
-    
-    //    //commented out
-    //    func loadRecordingUI() {
-    //        recordButton.translatesAutoresizingMaskIntoConstraints = false
-    //        recordAudio(recordButton)
-    //    }
-    
-    
-    // if audio recording fails
-    func loadFailUI() {
-        let failLabel = UILabel()
-        failLabel.font = UIFont.preferredFont(forTextStyle: .headline)
-        failLabel.text = "Recording failed: ensure the app has access to your microphone."
-        failLabel.numberOfLines = 0
-        
-        stackView.addArrangedSubview(failLabel)
-    }
-    
-    // stores audio in writable directory in app
-    class func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let documentsDirectory = paths[0]
-        return documentsDirectory
-    }
-    
-    class func getAudioURL() -> URL {
-        return getDocumentsDirectory().appendingPathComponent("audio.m4a")
-    }
-    
-    //
-    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        if !flag {
-            finishRecording(success: false)
-        } else { // should be called when 60 seconds is up
-            view.backgroundColor = UIColor(red: 0.6, green: 0, blue: 0, alpha: 1)
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // play recording
-    @IBAction func playButton(_ sender: UIButton) {
-        //        if (playButton.isEnabled) {
-        //            audioPlayer?.stop();
-        //            return;
-        //        }
-        recordButton.isEnabled = false
-        
-        audioPlayer?.prepareToPlay()
-        audioPlayer = try? AVAudioPlayer(contentsOf: AddEntryViewController.getAudioURL())
-        audioPlayer?.delegate = self
-        audioPlayer?.volume = 5.0
-        audioPlayer?.play()
-        recordButton.isEnabled = true;
-        playButton.isEnabled = true;
-    }
-    
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        recordButton.isEnabled = true
-        playButton.isEnabled = true
-    }
-    
-    @IBAction func onPhotoButton(_ sender: Any) {
-        imagePickerController = UIImagePickerController()
-        imagePickerController.delegate = self
-        imagePickerController.sourceType = .camera
-        present(imagePickerController, animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        imagePickerController.dismiss(animated: true, completion: nil)
-        selectedImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-    }
-    
-    func saveImage(imageName: String){
-        let _ = FileManager.default
-        //get the image path
-        let _ = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
-        //get the image we took with camera
-        let image = selectedImageView.image!
-        let _ = UIImagePNGRepresentation(image)
-        //store it in the document directory    fileManager.createFile(atPath: imagePath as String, contents: data, attributes: nil)
-    }
-    
-    func getImage(imageName: String){
-        let fileManager = FileManager.default
-        let imagePath = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imageName)
-        if fileManager.fileExists(atPath: imagePath){
-            selectedImageView.image = UIImage(contentsOfFile: imagePath)
-        }else{
-            print("ERROR: Could not find image :(")
-        }
-    }
-    
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-}
- */
-
